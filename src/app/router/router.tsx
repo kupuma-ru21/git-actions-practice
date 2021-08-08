9c9852e2fb4b37482c6fe0c3073a26cc6f390a0d
kupuma-ru21 <kupumaru21@gmail.com>
2021-08-08 22:53:15 +0900

    feat: add ZennArticle page

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from 'app/home';
+import ZennArticle from 'app/zenn-article';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/zenn_article" exact component={ZennArticle} />
       </Switch>
     </BrowserRouter>
   );
