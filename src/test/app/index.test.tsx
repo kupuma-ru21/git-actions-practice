d4417c18c13619b4c9a94749dd0408f3ca7d8d40
kupuma-ru21 <kupumaru21@gmail.com>
2021-08-08 22:41:39 +0900

    fix: test

--- src/test/app/index.test.tsx
+++ src/test/app/index.test.tsx
@@ -1,9 +1,20 @@
 import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
 import App from 'app/home';
+
+const makeSut = () => {
+  const history = createMemoryHistory({ initialEntries: ['/'] });
+  return render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
+};
 
 describe('ホーム', () => {
   test('レンダリング内容が正しく表示されてること', () => {
-    const { getByText } = render(<App />);
+    const { getByText } = makeSut();
     getByText('GitHub');
     getByText('Zenn');
   });
