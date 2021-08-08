13a9fda79babb8a7fa8a168a2800046555a5f57f
kupuma-ru21 <kupumaru21@gmail.com>
2021-08-08 23:06:31 +0900

    test: 「Homeに戻る」押下時、Home画面へ遷移すること

--- src/test/app/zenn-article/index.test.tsx
+++ src/test/app/zenn-article/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, RenderResult } from '@testing-library/react';
+import { fireEvent, render, RenderResult } from '@testing-library/react';
 import { createMemoryHistory, MemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
 import ZennArticle from 'app/zenn-article';
@@ -20,5 +20,11 @@ describe('「Zenn」記事一覧画面', () => {
   test('レンダリング内容が正しく表示されてること', () => {
     const { sut } = makeSut();
     sut.getByText('Zenn Article');
+  });
+
+  test('「Homeに戻る」押下時、Home画面へ遷移すること', () => {
+    const { sut, history } = makeSut();
+    fireEvent.click(sut.getByText('Homeに戻る'));
+    expect(history.location.pathname).toBe('/');
   });
 });
